0017e071119c929bacb2da7e48b093d190cf457b
lbarberiscanoni <hllbck7@gmail.com>
2015-09-04 11:00:25 -0700

    comipled the jade files nad renamed the folder to reflect its function

--- dbInterface/push.py
+++ dbInterface/push.py
@@ -3,7 +3,7 @@ import selenium
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get("http://lbarberiscanoni.github.io/smallCapAnalyzer/scriptsUsedToMakeTheDB/dbInterface.html")
+browser.get("http://lbarberiscanoni.github.io/smallCapAnalyzer/dbInterface/dbInterface.html")
 
 time.sleep(2)
 
